878892475c263f4f28a442bc597bb02e49036c00
Jakub Jan Strebeyko <strebeyko@gmx.com>
2020-04-19 10:27:40 +0200

    add first batch of exercizes

---
+++ zadania1.js
@@ -0,0 +1,26 @@
+/** 1.
+ * Zaimplementuj moduł Samochodu, który ma
+ * A) -wewnętrzną (prywatną) właściwość - ilość benzyny (liczba)
+ *    -wewnątrzną metodę spalania przjmującą za parametr ilość
+ *      i albo ją odejmującą od ilości benzyny, albo wyświetlającą
+ *      w konsoli wiadomość i pustym baku :(
+ * 
+ *  B) niech moduł zwraca obiekt z właściwością liczbową stacji radiowej,
+ *     metodą by ją zmienić oraz drugą służącą do jechania, przyjmującą
+ *     za parametr prędkość(liczba) i wykonywującą metodę z pkt. A 
+ *     
+ */
+
+ /**
+  *  Stwórz moduł - singleton. Niech posiada:
+  *
+  * A) wewnętrzny stan - listę produków
+  * 
+  * B) api:
+        publiczną metodę do dodawania produktu/ów
+        publiczną metodę do usuwania produktu/ów
+        publiczną metodę do wylistowania nazw produktów w stringu
+        publiczną metodę do usortowania produktów wg ceny
+        najważniejsze: niech pozwala na zsumowanie cen za produkty
+
+  */
